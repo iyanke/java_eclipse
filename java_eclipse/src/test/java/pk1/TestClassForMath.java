5f65a45c4971d77d10996da78f067df0e4ea3952
Inna.Yankelevich <inna_ya@mail.ru>
2012-07-26 18:38:06 +0400

    failed test failed

--- java_eclipse/src/test/java/pk1/TestClassForMath.java
+++ java_eclipse/src/test/java/pk1/TestClassForMath.java
@@ -13,7 +13,7 @@ public class TestClassForMath extends TestCase {
             System.out.print("2");
             System.out.print("hello");
             System.err.print("hello again");
-               Assert.assertEquals("Result", 30, tester.multiply(6, 5));
+               Assert.assertEquals("Result", 31, tester.multiply(6, 5));
 
         }
         public void testMultiply2() {
