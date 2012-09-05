27eed04ed297d7663c2aea307c12bc49c1e8692e
Inna.Yankelevich <inna_ya@mail.ru>
2012-09-05 16:33:45 +0400

    dsdsdsds

--- java_eclipse/src/test/java/pk1/TestClassForMath.java
+++ java_eclipse/src/test/java/pk1/TestClassForMath.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 
 public class TestClassForMath {
 
-        public void testMultiply() {
+@Ignore @Test       public void testMultiply() {
             ClassForMath tester = new ClassForMath();
             System.out.println("1");
             System.out.print("2");
@@ -50,7 +50,7 @@ public void failingtestSumma() {
 }
 
 
-public void testDifference2() {
+@Ignore @Test public void testDifference2() {
 //sdfsfdfsdfdfsdf
 	//sdfsdfsdfsdfsdf
 	//dfkdkfjskfjsldkfjslkdjflskjflksdjfsf
@@ -59,16 +59,16 @@ public void testDifference2() {
 assertEquals("Result", 50, tester.multiply(10, 5));
 
 }
-public void testDifference1() {
+@Ignore @Test public void testDifference1() {
     ClassForMath tester = new ClassForMath();
        assertEquals("Result", 7, tester.difference(12, 5));
 
 }
-public void testDivision() {
+@Ignore @Test public void testDivision() {
     ClassForMath tester = new ClassForMath();
        assertEquals("Result", 8, tester.division(8,1));}
 
-public void testDoubleSumma() {
+@Ignore @Test public void testDoubleSumma() {
 	//dfdfdfsfsdfsfdsfsf
 	//dfsfdfsdfdfsdfsff
     ClassForMath tester = new ClassForMath();
@@ -76,7 +76,7 @@ public void testDoubleSumma() {
        //zzxzxzxzxzxzxzxzxzxzx
        
 }
-public void testTripleSumma()
+@Ignore @Test public void testTripleSumma()
 {ClassForMath test1= new ClassForMath();
 assertEquals("Result", 12, test1.triple_summa(2,2) );
 	}
