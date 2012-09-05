073d730b9174bd3f885f1b39c967187c332f5b1e
Inna.Yankelevich <inna_ya@mail.ru>
2012-09-05 16:37:19 +0400

    rr

--- java_eclipse/src/test/java/pk1a/TestClassForMath.java
+++ java_eclipse/src/test/java/pk1a/TestClassForMath.java
@@ -1,22 +1,24 @@
 package test.java.pk1a;
 
 import main.java.pk1.ClassForMath;
-import junit.framework.*;
-//fff\rttttttttttttttttt
-////yyyyy
-//tg
+//import junit.framework.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 
-public class TestClassForMath extends TestCase {
+public class TestClassForMath {
 
-        public void testMultiply() {
+@Ignore @Test       public void testMultiply() {
             ClassForMath tester = new ClassForMath();
             System.out.println("1");
             System.out.print("2");
             System.out.print("hello");
             System.err.print("hello again");
-               Assert.assertEquals("Result", 30, tester.multiply(6, 5));
+               assertEquals("Result", 30, tester.multiply(6, 5));
 
         }
         public void testMultiply2() {
@@ -24,52 +26,59 @@ public class TestClassForMath extends TestCase {
             System.out.print("2");
             System.out.print("hello");
             System.err.print("hello again");
-               Assert.assertEquals("Result", 30, tester.multiply2(6, 5));
+               assertEquals("Result", 30, tester.multiply2(6, 5));
        }
 ////t
 
-public void testSumma() {
+
+public void failingtestSumma() {
 	
     ClassForMath tester = new ClassForMath();
     System.out.println("3");
     
-       Assert.assertEquals("Result", 17, tester.summa(12, 5));
+       assertEquals("Result", 17, tester.summa(12, 5));
 //dddddd
 }
-public void testDifference() {
+
+@Ignore @Test  public void testDifference() {
+
+
     ClassForMath tester = new ClassForMath();
     System.out.println("4");
-       Assert.assertEquals("Result", 7, tester.difference(12, 5));
+       assertEquals("Result", 17, tester.difference(12, 5));
 
 }
-public void testDifference2() {
+
+
+@Ignore @Test public void testDifference2() {
 //sdfsfdfsdfdfsdf
 	//sdfsdfsdfsdfsdf
 	//dfkdkfjskfjsldkfjslkdjflskjflksdjfsf
     ClassForMath tester = new ClassForMath();
-       Assert.assertEquals("Result", 7, tester.difference(12, 5));
+       assertEquals("Result", 7, tester.difference(12, 5));
+assertEquals("Result", 50, tester.multiply(10, 5));
 
 }
-public void testDifference1() {
+@Ignore @Test public void testDifference1() {
     ClassForMath tester = new ClassForMath();
-       Assert.assertEquals("Result", 7, tester.difference(12, 5));
+       assertEquals("Result", 7, tester.difference(12, 5));
 
 }
-public void testDivision() {
+@Ignore @Test public void testDivision() {
     ClassForMath tester = new ClassForMath();
-       Assert.assertEquals("Result", 8, tester.division(8,1));}
+       assertEquals("Result", 8, tester.division(8,1));}
 
-public void testDoubleSumma() {
+@Ignore @Test public void testDoubleSumma() {
 	//dfdfdfsfsdfsfdsfsf
 	//dfsfdfsdfdfsdfsff
     ClassForMath tester = new ClassForMath();
-       Assert.assertEquals("Result", 34, tester.double_summa(12, 5));
+       assertEquals("Result", 34, tester.double_summa(12, 5));
        //zzxzxzxzxzxzxzxzxzxzx
        
 }
-public void testTripleSumma()
+@Ignore @Test public void testTripleSumma()
 {ClassForMath test1= new ClassForMath();
-Assert.assertEquals("Result", 12, test1.triple_summa(2,2) );
+assertEquals("Result", 12, test1.triple_summa(2,2) );
 	}
 
 
