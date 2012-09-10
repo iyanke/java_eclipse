ad7116bee63e06a288d8062650c2e4afd61b10b5
unknown <Inna.Yankelevich@st-003.Labs.IntelliJ.Net>
2012-09-10 16:29:23 +0400

    ccccc

--- java_eclipse/src/test/java/pk1/TestClassForMath.java
+++ java_eclipse/src/test/java/pk1/TestClassForMath.java
@@ -56,7 +56,7 @@ public void testDifference() {
        assertEquals("Result", 7, tester.difference(12, 5));
 
 }
-public void testDifference2() {
+@Test @Ignore public void testDifference2() {
 //sdfsfdfsdfdfsdf
 	//sdfsdfsdfsdfsdf
 	//dfkdkfjskfjsldkfjslkdjflskjflksdjfsf
@@ -64,16 +64,16 @@ public void testDifference2() {
        assertEquals("Result", 7, tester.difference(12, 5));
 
 }
-public void testDifference1() {
+@Test @Ignore public void testDifference1() {
     ClassForMath tester = new ClassForMath();
        assertEquals("Result", 7, tester.difference(12, 5));
 
 }
-public void testDivision() {
+@Test @Ignore public void testDivision() {
     ClassForMath tester = new ClassForMath();
        assertEquals("Result", 8, tester.division(8,1));}
 
-public void testDoubleSumma() {
+@Test @Ignore public void testDoubleSumma() {
 	//dfdfdfsfsdfsfdsfsf
 	//dfsfdfsdfdfsdfsff
     ClassForMath tester = new ClassForMath();
@@ -81,7 +81,7 @@ public void testDoubleSumma() {
        //zzxzxzxzxzxzxzxzxzxzx
        
 }
-public void testTripleSumma()
+@Test @Ignore public void testTripleSumma()
 {ClassForMath test1= new ClassForMath();
 assertEquals("Result", 12, test1.triple_summa(2,2) );
 	}
